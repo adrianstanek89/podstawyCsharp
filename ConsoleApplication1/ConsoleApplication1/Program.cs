d7f1f73b6fb86d2f6a900d7c5af3110269a83c71
dasdas asdas <sad asd@dasd.pl>
2017-09-03 15:39:57 +0200

    a

--- ConsoleApplication1/ConsoleApplication1/Program.cs
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -11,6 +11,7 @@ namespace ConsoleApplication1
        
         static void Main(string[] args)
         {
+            int login, haslo;
             Console.WriteLine("Wprowadz Login");
             Console.ReadLine();
             
