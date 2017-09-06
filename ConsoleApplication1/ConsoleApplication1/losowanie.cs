28f7cc3348c775331c9e57826a41c092671b9598
Adrian Stanek <adrianek289tcz@gmail.com>
2017-09-06 10:57:26 +0200

    s

--- ConsoleApplication1/ConsoleApplication1/losowanie.cs
+++ ConsoleApplication1/ConsoleApplication1/losowanie.cs
@@ -11,8 +11,12 @@ namespace ConsoleApplication1
         public int zakres(int min, int max)
         {
             Random liczby = new Random();
-            int a = liczby.Next(0, 1000);
-            
+           
+            int num1 = liczby.Next(0, 1000);
+            int num2 = liczby.Next(0, 1000);
+            Console.WriteLine($"First random number: {num1}. Second random number: {num2}");
+            Console.WriteLine($"{num1} + {num2} = {num1 + num2}");
+
             Console.WriteLine();
             Console.Read();
         }
