8290eba17d56455536f0730b23478688f6ca173a
Adrian Stanek <adrianek289tcz@gmail.com>
2017-09-06 11:15:06 +0200

    losowanie

--- ConsoleApplication1/ConsoleApplication1/Program.cs
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -10,7 +10,21 @@ namespace ConsoleApplication1
     {
         static void Main(string[] args)
         {
-            
+            poczatek:
+            Random liczby = new Random();
+
+            int sum = 0;
+
+            for (int i = 0; i < 50; i++)
+            {
+                int liczba = liczby.Next(0, 1000);
+                sum += liczba;
+                Console.WriteLine("----> {0}", liczba);
+            }
+
+            Console.WriteLine("suma: {0}", sum); Console.Read();
+            Console.Clear();
+            goto poczatek;
         }
     }
 }
