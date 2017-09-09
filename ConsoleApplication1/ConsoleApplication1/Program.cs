b3440c6cf13696f51fca1a7d08413799380675c4
dasdas asdas <sad asd@dasd.pl>
2017-09-09 09:34:19 +0200

    zadanie liczby

--- ConsoleApplication1/ConsoleApplication1/Program.cs
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -3,21 +3,87 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
 namespace ConsoleApplication1
 {
     class Program
     {
+
         static void Main(string[] args)
         {
-            int a, b, wynik;
-            Console.Write("Podaj pierwsza liczbe: ");
-            a = int.Parse(Console.ReadLine());
-            Console.Write("Podaj druga liczbe :");
-            b = int.Parse(Console.ReadLine());
-            wynik = a + b;
-            Console.WriteLine("Wynik: " + wynik);
-            Console.ReadLine();
+            short a, b;
+            Console.Write("Podaj wartosc min w przedziale od 0 do 9 : ");
+            a = short.Parse(Console.ReadLine());
+            do
+            {
+                if (0 > a || 10 < a)
+                {
+                    Console.WriteLine("Podałes zla wartosc :");
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("Musi to byc liczba z przedziału 0-9");
+                    Console.ForegroundColor = ConsoleColor.White;
+                    Console.ReadKey();
+                    Console.Clear();
+                    Console.Write("Podaj wartosc min w przedziale od 0 do 9 : ");
+                    a = short.Parse(Console.ReadLine());
+
+                }
+                else if (0 <= a && a < 10)
+                {
+                    goto one;
+                }
+            }
+            while (true);
+            one:
+            Console.Write("Podaj wartosc w przedziale 9-99: ");
+            b = short.Parse(Console.ReadLine());
+            do
+            {
+                if (10 > b || 99 < b)
+                {
+                    Console.WriteLine("Podałes zla wartosc :");
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("Musi to byc liczba z przedziału 9-99");
+                    Console.ForegroundColor = ConsoleColor.White;
+                    Console.ReadKey();
+                    Console.Clear();
+                    Console.Write("Podaj wartosc w przedziale 9-99: ");
+                    b = short.Parse(Console.ReadLine());
+                }
+                else if (10 <= b && b < 100)
+                {
+                    goto two;
+                }
+            }
+            while (true);
+            two:
+            int parz = 0;
+            int zakres = 0;
+            int[] tablica = new int[1000];
+            Random liczby = new Random();
+            for (int i = 0; i < tablica.Length;)
+            {
+                zakres = liczby.Next(a, b);
+                // Console.WriteLine("cyfry: " + zakres);
+                tablica[i] = zakres;
+                i++;
+
+            }
+            int[] tablica2 = new int[50];
+            Array.Copy(tablica, 0, tablica2, 0, 50);
+
+            foreach (int element in tablica2)
+            {
+                // Console.WriteLine("liczba: "+ element); 
+                if (element % 2 == 0)
+                {
+
+                    // Console.WriteLine("parzysta: "+ element); 
+                    parz += element;
+                    Console.WriteLine("suma liczb parzystych: " + parz);
+                }
+            }
+
+            Console.ReadKey();
         }
     }
 }
